a14ec47df1c510b9cd7c4ef6b6e3a9d40394fbb0
anshuman <anshuman.sugandhi7816@gmail.com>
2026-03-03 11:57:32 +0530

    deepseek integration

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from api.views import analyze_website
-from . import views
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
